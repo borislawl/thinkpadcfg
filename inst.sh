35e12bf1c1d8aae018ae095f3ba088b3d18b07c5
popo <mmg@lol.net>
2021-07-11 15:01:49 -0400

    ...

---
+++ inst.sh
@@ -0,0 +1,4 @@
+sudo mv tlp.conf /etc/tlp.conf
+sudo mv thinkfan.com /etc/thinkfan.conf
+sudo cp 10-trackpoint.rules /etc/udev/rules.d/10-trackpoint.rules
+chown root:root /etc/udev/rules.d/10-trackpoint.rules
