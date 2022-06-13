3459e8be8d96dfb4d9981d9f4cca96784b238afe
me <me@w530>
2022-06-13 06:51:44 +0000

    fix

--- inst.sh
+++ inst.sh
@@ -1,4 +1,4 @@
 sudo cp tlp.conf /etc/tlp.conf
-sudo cp thinkfan.com /etc/thinkfan.conf
+sudo cp thinkfan.conf /etc/thinkfan.conf
 sudo cp 10-trackpoint.rules /etc/udev/rules.d/10-trackpoint.rules
-sudo chown root:root /etc/udev/rules.d/10-trackpoint.rules
+#sudo chown root:root /etc/udev/rules.d/10-trackpoint.rules
